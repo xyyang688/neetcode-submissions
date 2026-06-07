ec911b26ab10c0d7e9a5185c9701c23b7ac6a66d
Xinyue Yang <xyyang@unc.edu>
2026-06-07 19:04:46 -0400

    Add: number-of-one-bits - submission-3

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while(n != 0){
+            if ((n&1) == 1){
+                res += 1;
+            }
+            n= n>>>1;
+        }
+
+        return res;
+    }
+}
